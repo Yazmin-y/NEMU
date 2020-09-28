e37488fddb00e6ad0485510524e21caddc43685b
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 01:44:02 -0400

    >  compile NEMU 3019244132 yazmin Linux yazmin-VirtualBox 5.3.0-28-generic #30~18.04.1-Ubuntu SMP Fri Jan 17 06:14:09 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:44:02 up  6:11,  1 user,  load average: 0.24, 0.24, 0.25 b8bfca1a25f2c993b6d5b814a10885c13f06539

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -87,7 +87,7 @@ static bool make_token(char *e) {
 				int substr_len = pmatch.rm_eo;
 				char *tmp = e + position + 1;
 				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
-				position += substr_len;
+				
 
 				/* TODO: Now a new token is recognized with rules[i]. Add codes
 				 * to record the token in the array `tokens'. For certain types
