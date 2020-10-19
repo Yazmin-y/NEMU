1e02336ffd08142022ad4c92703e46fc75566337
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-19 04:44:23 -0400

    >  compile NEMU 3019244132 yazmin Linux yazmin-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:44:23 up 21:44,  1 user,  load average: 0.74, 0.43, 0.39 7e85f44dd4ccdfeaffdc39d93015a922ff6dcbd

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -3,32 +3,11 @@
 #define instr lods
 
 static void do_execute() {
-    if (ops_decoded.is_operand_size_16)
-    {
-        swaddr_write(reg_w(R_SI), 2, reg_w(R_AX));
-        if (cpu.DF == 0)
-        {
-            reg_w(R_DI) += DATA_BYTE;
-        } else
-        {
-            reg_w(R_DI) -= DATA_BYTE;
-        }
-    }
-    else
-    {
-        swaddr_write(reg_l(R_ESI), 4, reg_l(R_EAX));
-        if (cpu.DF == 0)
-        {
-            reg_l(R_ESI) += DATA_BYTE;
-        } else
-        {
-            reg_l(R_ESI) -= DATA_BYTE;
-        }
-    }
+    REG(R_EAX) = swaddr_read(reg_l(R_ESI), DATA_BYTE);
+    if (cpu.DF == 0) reg_l(R_ESI) += DATA_BYTE;
+    else reg_l(R_ESI) -= DATA_BYTE;
 
-    print_asm("lods");
-    
-    
+    print_asm("lods%s", str(SUFFIX));
 }
 
 make_instr_helper(n);
