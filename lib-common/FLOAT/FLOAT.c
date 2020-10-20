6bd6d0120dceb49e379fd33254e9eb9811445ed9
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 08:09:06 -0400

    >  compile NEMU 3019244132 yazmin Linux yazmin-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:09:06 up 1 day,  1:34,  1 user,  load average: 0.45, 0.51, 0.45 c8b9ab1b916187666b96c5568c095faa83a4f679

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,8 +1,8 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	long long c = (long long)a * (long long)b; 
+	return (FLOAT)(c >> 16);
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
@@ -24,8 +24,33 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 	 * out another way to perform the division.
 	 */
 
-	nemu_assert(0);
-	return 0;
+	int sign = 1;
+	if (a < 0)
+	{
+		sign = -sign;
+		a = -a;
+	}
+	if (b < 0)
+	{
+		sign = -sign;
+		b = -b;
+	}
+	int result = a/b;
+	a = a%b;
+	int i;
+	for (i = 0; i < 16; i++)
+	{
+		a <<= 1;
+		result <<= 1;
+		if (a >= b)
+		{
+			a -= b;
+			result ++;
+		}
+		
+	}
+	
+	return result*sign;
 }
 
 FLOAT f2F(float a) {
@@ -39,13 +64,24 @@ FLOAT f2F(float a) {
 	 * performing arithmetic operations on it directly?
 	 */
 
-	nemu_assert(0);
+	int b = *(int *)&a;
+	int sign = b >> 31;
+	int exp = (b >> 23) & 0xff;
+	FLOAT k = b&0x7fffff;
+	if (exp != 0) k += 1 << 23;
+	exp -= 150;
+	if (exp < -16) k >>= -16-exp;
+	if (exp > -16) k <<= exp+16;
+	return sign == 0 ? k : -k;
+	
 	return 0;
 }
 
 FLOAT Fabs(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	FLOAT b;
+	if (a < 0) b = -a;
+	else b = a;
+	return b;
 }
 
 /* Functions below are already implemented */
