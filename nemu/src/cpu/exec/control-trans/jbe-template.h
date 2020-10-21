667c0fc14843045bcb14cf2975a00ecf55f0e665
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 06:33:57 -0400

    >  compile NEMU 3019244132 yazmin Linux yazmin-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:33:57 up 1 day,  6:26,  1 user,  load average: 0.39, 0.33, 0.28 54f22801172ff56b9c7304d665a3854398c51bb

--- nemu/src/cpu/exec/control-trans/jbe-template.h
+++ nemu/src/cpu/exec/control-trans/jbe-template.h
@@ -4,8 +4,8 @@
 
 static void do_execute() {
     DATA_TYPE_S displacement = op_src->val;
-    print_asm("ja %x", cpu.eip + 1 + DATA_BYTE + displacement);
-    if (cpu.ZF == 1 && cpu.CF == 1) cpu.eip += displacement;
+    print_asm("jbe %x", cpu.eip + 1 + DATA_BYTE + displacement);
+    if (cpu.ZF == 1 || cpu.CF == 1) cpu.eip += displacement;
 }
 
 make_instr_helper(i)
