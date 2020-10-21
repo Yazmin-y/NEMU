bc7f604aaaf7a2ebc547b9baaee8e6c343c2f051
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 06:30:51 -0400

    >  compile NEMU 3019244132 yazmin Linux yazmin-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:30:51 up 1 day,  6:23,  1 user,  load average: 0.63, 0.44, 0.30 7a2c237259a524482366bd1b1d532addb56a4821

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -4,10 +4,18 @@
 
 static void do_execute () {
 	DATA_TYPE result = op_dest->val & op_src->val;
-	OPERAND_W(op_dest, result);
+	
 
-	cpu.CF = cpu.OF = 0;
-	concat(update_, SUFFIX) (result);
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF = 0;
+	cpu.OF = 0;
+	cpu.SF = result >> len;
+	cpu.ZF = !result;
+	OPERAND_W(op_dest, result);
+	result ^= result >> 4;
+	result ^= result >> 2;
+	result ^= result >> 1;
+	cpu.PF = !(result & 1);
 
 	print_asm_template2();
 }
