e7db4f035e8a83fa6fcd208b89484bdcb9548a80
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 07:37:25 -0400

    >  compile NEMU 3019244132 yazmin Linux yazmin-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:37:25 up 1 day,  7:29,  1 user,  load average: 0.98, 0.76, 0.87 a507e65f5eabf6359dfae3af5db66f801a7c769c

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -14,7 +14,7 @@ make_helper(concat(scas_n_, SUFFIX)) {
     concat(update_, SUFFIX) (res);
     int len = (DATA_BYTE << 3) - 1;
     cpu.CF = s1<s2;
-    cpu.OF = ((s1>>len) != (s2>>len) && (s2>>len) == cpu.SF);
+    cpu.OF = (((s1>>len) != (s2>>len)) && ((s2>>len) == cpu.SF));
     print_asm("scas%s", str(SUFFIX));
 
     return 1;
