d17550b2456caed101b35854ad5d1eb24c9ad05f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 03:32:54 -0400

    >  compile NEMU 3019244132 yazmin Linux yazmin-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:32:54 up 1 day,  3:38,  1 user,  load average: 0.24, 0.13, 0.17 aeea862256011c3d04afe83b63e745016ea033f

--- nemu/src/cpu/exec/control-trans/ret-template.h
+++ nemu/src/cpu/exec/control-trans/ret-template.h
@@ -3,7 +3,7 @@
 #define instr ret
 
 make_helper(concat(ret_n_, SUFFIX)) {
-    cpu.eip = MEM_R(reg_l(R_ESP)) - 1;
+    cpu.eip = MEM_R(reg_l(R_ESP));
     reg_l(R_ESP) += DATA_BYTE;
     print_asm("ret");
     return 1;
