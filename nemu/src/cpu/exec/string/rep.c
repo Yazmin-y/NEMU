da5a920439b0fb2da4d5e3faf320406814cdfd96
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 03:53:03 -0400

    >  compile NEMU 3019244132 yazmin Linux yazmin-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:53:03 up 1 day,  3:58,  1 user,  load average: 0.37, 0.25, 0.26 743b063580a708ec41389645dc6c25b0608e

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -25,7 +25,11 @@ make_helper(rep) {
 				|| ops_decoded.opcode == 0xaf	// scasw
 				); */
 			
-			if ((ops_decoded.opcode == 0xa6
+			if ((ops_decoded.opcode == 0xa4	// movsb
+				|| ops_decoded.opcode == 0xa5	// movsw
+				|| ops_decoded.opcode == 0xaa	// stosb
+				|| ops_decoded.opcode == 0xab
+				|| ops_decoded.opcode == 0xa6
 				|| ops_decoded.opcode == 0xa7
 				|| ops_decoded.opcode == 0xae
 				|| ops_decoded.opcode == 0xaf) 
@@ -67,7 +71,7 @@ make_helper(repnz) {
 				|| ops_decoded.opcode == 0xa7
 				|| ops_decoded.opcode == 0xae
 				|| ops_decoded.opcode == 0xaf) 
-				&& cpu.ZF == 0)
+				&& cpu.ZF == 1)
 			{
 				break;
 			}
