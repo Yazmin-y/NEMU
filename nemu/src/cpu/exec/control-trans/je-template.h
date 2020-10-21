7578dfd307f97aed607426f180fa5e074f74da39
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 05:34:09 -0400

    >  compile NEMU 3019244132 yazmin Linux yazmin-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:34:09 up 1 day,  5:39,  1 user,  load average: 0.37, 0.31, 0.30 dbc73e79a14aa993a02ec925226244c852acae06

--- nemu/src/cpu/exec/control-trans/je-template.h
+++ nemu/src/cpu/exec/control-trans/je-template.h
@@ -2,16 +2,11 @@
 
 #define instr je
 
-static void do_execute() {
-    DATA_TYPE_S displacement = op_src->val;
-    print_asm("je %x", cpu.eip+1+DATA_BYTE+displacement);
-    if (cpu.ZF == 1)
-    {
-        cpu.eip += displacement;
-    }
-    
+make_helper(concat(je_i_, SUFFIX)) {
+    int len = concat(decode_i_, SUFFIX)(eip+1);
+    print_asm("je 0x%x", (DATA_TYPE_S)op_src->imm + cpu.eip + len + 1);
+    if (cpu.ZF == 1) cpu.eip += (DATA_TYPE_S)op_src->imm;
+    return len+1;
 }
-
-make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"
