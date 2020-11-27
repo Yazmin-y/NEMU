3b56ac986ab88e8b1e8c0b1c59b646a5d8e70847
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-27 05:45:01 -0500

    >  compile NEMU 3019244132 yazmin Linux yazmin-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:45:01 up 2 days,  6:17,  1 user,  load average: 0.44, 0.30, 0.25 da291e0d273986cb7b63b5fcf8e8d4979f8885b

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -1,12 +1,15 @@
 #include "common.h"
 #include "memory.h"
 #include <string.h>
+#include "x86-inc/mmu.h"
 
 #define VMEM_ADDR 0xa0000
 #define SCR_SIZE (320 * 200)
+// #define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE)
 
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
+static PTE vmem_pt[1024] align_to_page;
 
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
@@ -14,7 +17,18 @@ void create_video_mapping() {
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	panic("please implement me");
+	// panic("please implement me");
+	PDE *pdp = (PDE *)va_to_pa(get_updir());
+	pdp[0].page_frame = (uint32_t) va_to_pa(vmem_pt) >> 12;
+	pdp[0].present = 1;
+	int i;
+	for ( i = VMEM_ADDR / PAGE_SIZE; i < (VMEM_ADDR + SCR_SIZE)/PAGE_SIZE; i++)
+	{
+		vmem_pt[i].page_frame = i;
+		vmem_pt[i].present = 1;
+	}
+	
+
 }
 
 void video_mapping_write_test() {
